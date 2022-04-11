66cbf990d650b47c1f9abef8400b6eff85ce2dc2
surene <mysurene@gmail.com>
2022-04-11 09:39:25 +0800

    init

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.tencent.wxcloudrun.controller;
 
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * index控制器
@@ -17,6 +21,14 @@ public class IndexController {
   @GetMapping
   public String index() {
     return "index";
+  }
+
+  @RequestMapping("test")
+  @ResponseBody
+  public String test() throws JSONException {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("test","testdata");
+    return jsonObject.toString();
   }
 
 }
