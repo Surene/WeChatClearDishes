1ba9c2c30661466dd1b2b11cb7af354847f53515
surene <mysurene@gmail.com>
2022-04-14 15:01:13 +0800

    api/category

--- src/main/java/com/tencent/wxcloudrun/dao/CategoryMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/CategoryMapper.java
@@ -2,7 +2,9 @@ package com.tencent.wxcloudrun.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tencent.wxcloudrun.model.Category;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface CategoryMapper extends BaseMapper<Category> {
 
 }
--- src/main/java/com/tencent/wxcloudrun/model/Category.java
+++ src/main/java/com/tencent/wxcloudrun/model/Category.java
@@ -1,8 +1,10 @@
 package com.tencent.wxcloudrun.model;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("Category")
 public class Category {
     private Integer id;
     private String cateName;
